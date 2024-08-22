2abc029627b97f740ab684245f5a411700ded6e8
manishabhongal <bhongalmanisha@gmail.com>
2024-08-22 18:00:40 +0530

    [manisha] changes done in line no 7 and added new tc url1

--- src/test/java/testClasses/MAN_TC_002_verifyURL.java
+++ src/test/java/testClasses/MAN_TC_002_verifyURL.java
@@ -8,4 +8,10 @@ public class MAN_TC_002_verifyURL {
   public void verifyURL() {
 	  Reporter.log("URL verified", true);
   }
+  
+  
+  @Test
+  public void verifyURL1() {
+	  Reporter.log("url1 verified and done changes", true);
+  }
 }
