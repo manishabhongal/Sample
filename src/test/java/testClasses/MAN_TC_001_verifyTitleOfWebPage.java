fb3bb6f0f79d44a3135be478814b1ed9b75de1a7
manishabhongal <bhongalmanisha@gmail.com>
2024-08-22 16:09:34 +0530

    [Manisha] This is my Project

---
+++ src/test/java/testClasses/MAN_TC_001_verifyTitleOfWebPage.java
@@ -0,0 +1,11 @@
+package testClasses;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class MAN_TC_001_verifyTitleOfWebPage {
+  @Test
+  public void verifyTitleOfWebPage() {
+	  Reporter.log("Verifying Title of the WebPage", true);
+  }
+}
