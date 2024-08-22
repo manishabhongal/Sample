8ac3cda3d99d89460a7d96570aa88a9dd73cd43b
manishabhongal <bhongalmanisha@gmail.com>
2024-08-22 17:44:26 +0530

    [Manisha] new TC added and updated code

--- src/test/java/testClasses/MAN_TC_002_verifyURL.java
+++ src/test/java/testClasses/MAN_TC_002_verifyURL.java
@@ -1,9 +1,11 @@
 package testClasses;
 
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class MAN_TC_002_verifyURL {
   @Test
-  public void f() {
+  public void verifyURL() {
+	  Reporter.log("URL verified", true);
   }
 }
